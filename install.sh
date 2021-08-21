3ba724c80f2d3c03ff5b1a3520a597ce12ec06ea
evandro777 <evandro777@hotmail.com>
2021-08-21 18:44:38 -0300

    Install script: options for weather conky

--- install.sh
+++ install.sh
@@ -60,6 +60,36 @@ rm -r "${installLocation}seven-conky-main/"
 
 rm "${installLocation}main.zip"
 
+# WEATHER CONFIG
+weatherConfigLocation="${installLocation}/Weather/conky-weather/WeatherConfig.sh"
+cp "${installLocation}/Weather/conky-weather/WeatherConfig.sample.sh" "${weatherConfigLocation}"
+
+if [ -z "$weatherKey" ]; then
+	echo -e "${ORANGE}Open Weather Map API Key (To get one, register an account and create an api key on https://home.openweathermap.org/api_keys): ${NC}"
+	read -p "" weatherKey
+	sed -i 's/weatherKey=.*$/weatherKey="'$weatherKey'"/g' "${weatherConfigLocation}"
+fi
+
+if [ -z "$weatherCityId" ]; then
+	echo -e "${ORANGE}Open Weather City Id: ${NC}"
+	echo -e "To get the city id to get the weather, search your city id at: [https://openweathermap.org/], then copy the link of the city, something like: https://openweathermap.org/city/3457095 and use only the end number: 3457095"
+	read -p "" weatherCityId
+	sed -i 's/weatherCityId=.*$/weatherCityId="'$weatherCityId'"/g' "${weatherConfigLocation}"
+fi
+
+if [ -z "$weatherUnit" ]; then
+	echo -e "${ORANGE}Units [standard: Kelvin, metric: Celsius, imperial: Fahrenheit]: ${NC}"
+	read -p "" weatherUnit
+	sed -i 's/weatherUnit=.*$/weatherUnit="'$weatherUnit'"/g' "${weatherConfigLocation}"
+fi
+
+if [ -z "$weatherLanguage" ]; then
+	echo -e "${ORANGE}Language [en: English, pt_br: Português Brasil, sp, es: Spanish] (get a complete list at: https://openweathermap.org/current#multi): ${NC}"
+	read -p "" weatherLanguage
+	sed -i 's/weatherLanguage=.*$/weatherLanguage="'$weatherLanguage'"/g' "${weatherConfigLocation}"
+fi
+
+
 sevenConkyAutostartFile="${HOME}/.config/autostart/seven-conky.desktop"
 
 sevenConkyAutostartContent='[Desktop Entry]
