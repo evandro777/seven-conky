4c1d794fa80d8620f759e90cbc67e922944ba696
evandro777 <seven.skull@gmail.com>
2024-11-19 00:01:44 -0300

    Fixed Weather conky openweathermap: onecall is not free anymore, now using forecast api, but doesn't have alerts anymore

--- Weather/conky-weather/WeatherFetch.sh
+++ Weather/conky-weather/WeatherFetch.sh
@@ -4,8 +4,11 @@ cd "$(dirname "$(realpath "$0")")"
 
 mkdir -p /tmp/conky-weather/cache/
 #curl -s "api.openweathermap.org/data/2.5/forecast/daily?APPID=$weatherKey&id=$weatherCityId&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/forecast.json
-curl -s "api.openweathermap.org/data/2.5/weather?APPID=$weatherKey&id=$weatherCityId&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/weather.json
-curl -s "api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&APPID=$weatherKey&lat="$(jq -r ".coord.lat" /tmp/conky-weather/cache/weather.json)"&lon="$(jq -r ".coord.lon" /tmp/conky-weather/cache/weather.json)"&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/onecall.json
+curl -s "api.openweathermap.org/data/2.5/weather?appid=$weatherKey&id=$weatherCityId&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/weather.json
+curl -s "api.openweathermap.org/data/2.5/forecast/daily?appid=$weatherKey&lat="$(jq -r ".coord.lat" /tmp/conky-weather/cache/weather.json)"&lon="$(jq -r ".coord.lon" /tmp/conky-weather/cache/weather.json)"&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/forecast.json
+
+# This one bellow is not on free tier anymore
+#curl -s "api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly&appid=$weatherKey&lat="$(jq -r ".coord.lat" /tmp/conky-weather/cache/weather.json)"&lon="$(jq -r ".coord.lon" /tmp/conky-weather/cache/weather.json)"&units=$weatherUnit&lang=$weatherLanguage" -o /tmp/conky-weather/cache/onecall.json
 
 # Copy weather image
 cp -f icons/$(jq .weather[0].id /tmp/conky-weather/cache/weather.json).png /tmp/conky-weather/cache/current.png
